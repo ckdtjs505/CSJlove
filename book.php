26994298ab9293c0aadd9cdf790227d4e6e8b1f1
ckdtjs505 <36597179+ckdtjs505@users.noreply.github.com>
2018-08-29 00:35:33 +0900

    Add files via upload

---
+++ book.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="kr" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3mobile.css">
+		<title>진선 커플</title>
+		<script src="https://cdn.ckeditor.com/ckeditor5/11.0.1/classic/ckeditor.js"></script>
+		<link rel="stylesheet" href="sets/css/main.css">
+		  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/earlyaccess/hanna.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="https://cdn.ckeditor.com/ckeditor5/11.0.1/classic/ckeditor.js"></script>
+	</head>
+	<body id ="main">
+		<section id="book" >
+		<nav class="navbar navbar-inverse">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#">CSJ couple</a>
+		    </div>
+		    <div class="collapse navbar-collapse" id="myNavbar">
+		      <ul class="nav navbar-nav">
+		        <li><a href="index.html">Home</a></li>
+		        <li class="active"><a href="#">books</a></li>
+		        <li><a href="#">Thanks</a></li>
+		      </ul>
+		    </div>
+		  </div>
+		</nav>s
+		<div id="wrapper">
+			<!-- <header id ="header">
+
+			</header> -->
+
+			<div class="panel-group" id="accordion">
+			  <div class="panel panel-default">
+			    <div class="panel-heading">
+			      <h4 class="panel-title">
+			        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+			        청년아 울더라도 뿌려야 한다</a>
+			      </h4>
+			    </div>
+			    <div id="collapse1" class="panel-collapse collapse in">
+			      <div class="panel-body">
+							<div id = "content">
+							<p><b>
+									크리스천과 선택 <br>
+									 그러나 몇 가지만 하든지 혹 한 가지만이라도 족하니라마리아는 이좋은 편을 택하였으니 빼앗기지 아니하리라 <br>(눅 10 :42)</b>
+								 </p> <br>
+
+							 <div id="main">
+								 <strong>Content</strong>
+									 <p>모든 것이 선택, 결단의 부제 <br>
+		 							인생은 선택과 결단의 연속이다. 따라서 선택은 매우 중요하다.선택하려는 그 하나만 보는 것이 아니라 선택에서 제외되는 나머지를 살펴야 한다.
+		 							상황에 따라 작은 것이 엉뚱하게 커 보일 때도 있고 반대로 큰 것이 형편없이 작게 보일 때도 있는 까닭이다.<br>
+		 							선택은 매우 중요하다. 신앙도 선택이다.<br>
+		 							<p>
+
+			 						 주님이 원하시는 선택을 하기 위해서는 유의해야 할 점
+			 						 <br>1.인물을 본으로 선택한다는 것은 그들에게 주어진 마지막 단 열매만을 취사 선택하는 것이 아니라 그들에게 있었던 좌절과 아픔 연단 고통까지도 수용하는 것을 의미한다.			 						 그 과정속에서 우리는 진정한 신앙인으로 세워진다.<br>
+
+			 						 좋은 결과 단 열매만을 가려 뽑아 자기 소유로 삼으로 한다면 그것은 신앙적인 선택일 수 없다 단순하 미신이다.<br><br>
+
+			 						 2.무엇을 선택하든 중심이 주님이라면 그 선택은 항상 옳다.<br>
+			 							나를 위해서가 아닌 하나님을 위해서 선택하는 것은 옳다.
+			 						 문제는 주님을 위해서가 아닌 나의 명예를 위해서 하는 것은 참 위험하다. 마리아와 마르다 두사람의 선택이 다옳다는 것을 알려주셨다. 주님을 위한 선택인 이상 그 역시 옳은 선택이라는 것이다. 다른 사람이 무엇을 선택하든 그것역시 주님을 위한 선택으로 존중해 줄 수 있다.<br><br>
+
+			 						 3.무엇을 선택하든 그 선택이 주님을 위한 것이라면 주님꼐서 반드시 결과를 책임져 주신다<br>
+			 						 중요한 것은 왜 선택했느냐가 중요하다. 그 해답이 주님이라면 우리의 모든 선택은 그 안에서 옳은 선택이 된다. 주님께서는 반드시 협력해서 선을 이루게 하셔서서 그 결과를 반드시 책임져 주신다.<br><br>
+
+			 						 4. 주님을 위한 선택이라면 그 선택에 대해 끝까지 소명의식을 지켜야한다.<br>
+			 						 기대했던 것과는 다른 사람이요 삶이다. 그럼에도 겨혼했다념 그 선택에 대한 소명의식을 끝까지 가져야한다. 자신들이 선택한 목회의 길에 대한 소명의식이 뚜렷하기 떄문이다.<br><br>
+
+			 						 5. 내가 주님을 위해 무엇을 선택하는 것도 중요하지만, 내가 주님의 선택을 받을 수 있도록 바르게 가꾸는 것이 더 중요하다.<br>
+			 						 사울의 경우 하나님의 선택보다도 왕자를 더 중하게 여겼다.
+			 						 자신의 힘으로 왕자를 지킬 수 있다는 착각이다.
+			 						 항상 하나님의 선택을 받을 수 있도록 바르게 가꾸는 삶을 선태했기 떄문이다. 하나님이 우리를 선택하셨고, 그 분 선택안에서 그분을 선택하자.<br>
+									 </p>
+
+							 </div>
+							 <div id="feel">
+									 <strong>느낀점</strong>
+									 하나님 앞에서 더 바르게 선택하기위해 하나님을 위한 선택을 해야겠다 또한 수진이와 만날 때. 나의 만족과 유익이 주님을 위해서 만나도록 같이 말씀나눔과, 결단, 사랑을 대화를 해야한다. <br><br>
+
+
+										<strong>적용점</strong>
+										 1) 하나님을 위해 선진 커플이 무엇을 할 수 있을까요?<br>
+											- 하나님의 은혜에 대해 매일 5감사<br>
+											- 하나님의 주신 신앙 서적읽고 바르게 삶을 가꾸어 나가기<br>
+											- 하나님이 주신 말씀을 매일 나누기<br>
+											- 하나님 앞에서 절제하기(시간,물질…)<br>
+											 (수진 曰: 내가 원하는 데로 한다면 그건 좋지 않다. 내가 마음을 내려놓고 하나님 앞에서 절제를 하는 것이 매우 중요하다 !)<br>
+							 </div>
+							</div>
+
+
+			      </div>
+			    </div>
+			  </div>
+			  <!-- <div class="panel panel-default">
+			    <div class="panel-heading">
+			      <h4 class="panel-title">
+			        <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+			        Collapsible Group 2</a>
+			      </h4>
+			    </div>
+			    <div id="collapse2" class="panel-collapse collapse">
+			      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+			      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+			      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+			      commodo consequat.</div>
+			    </div>
+			  </div>
+			  <div class="panel panel-default">
+			    <div class="panel-heading">
+			      <h4 class="panel-title">
+			        <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+			        Collapsible Group 3</a>
+			      </h4>
+			    </div>
+			    <div id="collapse3" class="panel-collapse collapse">
+			      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+			      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+			      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+			      commodo consequat.</div>
+			    </div>
+			  </div>
+			</div> -->
+			<textarea name="content" id="editor">
+				this is first name
+			</textarea>
+			<script>
+					ClassicEditor
+							.create( document.querySelector( '#editor' ) )
+							.catch( error => {
+									console.error( error );
+							} );
+			</script>
+			<button type="input" class="btn">제출</button>
+			</section>
+
+			<footer>
+				<ol>
+					made by : CS.
+
+				</ol>
+			</footer>
+		</div>
+
+
+	</body>
+</html>
